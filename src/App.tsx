28400cf688a92d2f0c5dfd5b21fc5103d4113f2a
Michal Gawik <gawlik.zator@o2.pl>
2024-09-22 17:23:37 +0200

    configure router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
-
-
+import Home from './pages/Home'
+import CreateTour from './pages/CreateTour'
+import UpdateTour from './pages/UpdateTour'
 
 function App() {
-	return
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/create' element={<CreateTour />} />
+				<Route path='/:id' element={<UpdateTour />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
