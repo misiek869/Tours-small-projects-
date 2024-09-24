6f388d831fea6afc98387aaa3207131105f3bfa2
Michal Gawik <gawlik.zator@o2.pl>
2024-09-24 05:45:53 +0200

    receive data and render it

--- src/components/SingleTour.tsx
+++ src/components/SingleTour.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react'
 import { type Tour } from '../App'
 
 interface SingleTourProps {
@@ -5,9 +6,34 @@ interface SingleTourProps {
 }
 
 const SingleTour = ({ tour }: SingleTourProps) => {
-	return <div>{tour.name}</div>
+	const [readMore, setReadMore] = useState<boolean>(false)
+
+	const { name, image, info, price, id } = tour
+
+	return (
+		<article className='single-tour'>
+			<img src={image} alt={name} className='img' />
+			<span className='tour-price'>${price}</span>
+			<div className='tour-info'>
+				<h4>{name}</h4>
+
+				<p>
+					{readMore ? info : `${info.substring(0, 200)}...`}
+
+					<button onClick={() => setReadMore(!readMore)} className='info-btn'>
+						{readMore ? 'show less' : 'read more'}
+					</button>
+				</p>
+				<p>{price}</p>
+				<button
+					onClick={() => removeTour(id)}
+					className='delete-btn btn-block btn'
+					type='button'>
+					Remove Tour
+				</button>
+			</div>
+		</article>
+	)
 }
-
-
 
 export default SingleTour
