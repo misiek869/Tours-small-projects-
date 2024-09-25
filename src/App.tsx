6a5563fc3e2154a06618b183617f0fd317ac5ecf
Michal Gawik <gawlik.zator@o2.pl>
2024-09-25 05:28:47 +0200

    add more logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import supabase from './config/supabaseClient'
 import Tours from './components/Tours'
+import Loading from './components/Loading'
 
 export type Tour = {
 	id: number
@@ -12,6 +13,12 @@ export type Tour = {
 
 function App() {
 	const [tours, setTours] = useState<Tour[]>([])
+	const [loading, setLoading] = useState<boolean>(true)
+
+	const removeTour = id => {
+		const newTours = tours.filter(tour => tour.id !== id)
+		setTours(newTours)
+	}
 
 	const fetchData = async (): Promise<Tour[]> => {
 		const { data, error } = await supabase.from('tours').select()
@@ -20,13 +27,35 @@ function App() {
 			console.error('Error fetching data:', error)
 			return []
 		}
-		// return data as Tour
+		setLoading(false)
 		setTours(data)
+		return data
 	}
 
 	useEffect(() => {
 		fetchData()
 	}, [])
+
+	if (loading) {
+		return (
+			<main>
+				<Loading />
+			</main>
+		)
+	}
+
+	if (tours.length === 0) {
+		return (
+			<main>
+				<div className='title'>
+					<h2>no tours left</h2>
+					<button className='btn' onClick={() => fetchData()}>
+						refresh
+					</button>
+				</div>
+			</main>
+		)
+	}
 
 	return (
 		<main>
