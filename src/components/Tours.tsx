5ee042763939d453f1bd55d48e0dcff3fa09a5ba
Michal Gawik <gawlik.zator@o2.pl>
2024-09-25 05:17:36 +0200

    remove react query

--- src/components/Tours.tsx
+++ src/components/Tours.tsx
@@ -1,21 +1,6 @@
 import SingleTour from './SingleTour'
-import { fetchData } from '../App'
-import { useQuery } from '@tanstack/react-query'
 
 const Tours = () => {
-	const {
-		isPending,
-		isError,
-		error,
-		data: tours,
-	} = useQuery({
-		queryKey: ['tours'],
-		queryFn: fetchData,
-	})
-
-	if (isPending) return <h2>Loading...</h2>
-	if (isError) return <h2>Error : {error.message} </h2>
-
 	return (
 		<section>
 			<div className='title'>
